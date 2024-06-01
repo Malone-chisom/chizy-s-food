cb0e631b1fa3c984f45c0114139e7d8374073785
Malone-chisom <johnsonmalone007@gmail.com>
2024-06-01 05:13:43 +0100

    now

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pages from './components/pages/Pages'
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import LoginPopup from './components/loginPopup/LoginPopup';
 
 function App() {
+
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Pages />
       </div>
       <Footer />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './navbar.css';
 import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
     const [menue, setMenue] = useState("home")
 
     return (
@@ -10,10 +11,10 @@ const Navbar = () => {
             <img src={assets.logo} alt="logo" className="logo" />
 
             <ul className='navbar-menue'>
-                <li onClick={() => setMenue("home")} className={menue == "home" ? "active" : ""}>home</li>
-                <li onClick={() => setMenue("menue")} className={menue == "menue" ? "active" : ""}> menue</li>
-                <li onClick={() => setMenue("mobile-app")} className={menue == "mobile-app" ? "active" : ""}> mobile-app </li>
-                <li onClick={() => setMenue("contact-us")} className={menue == "contact-us" ? "active" : ""}> contact us</li>
+                <Link to="/" onClick={() => setMenue("home")} className={menue == "home" ? "active" : ""}>home</Link>
+                <a href='#app-download' onClick={() => setMenue("mobile-app")} className={menue == "mobile-app" ? "active" : ""}> mobile-app </a>
+                <a href='#explore-menu' onClick={() => setMenue("menue")} className={menue == "menue" ? "active" : ""}> menue</a>
+                <a href='#footer' onClick={() => setMenue("contact-us")} className={menue == "contact-us" ? "active" : ""}> contact us</a>
             </ul>
 
             <div className="navbar-right">
@@ -22,7 +23,7 @@ const Navbar = () => {
                     <img src={assets.basket_icon} alt="" />
                     <div className='dot'></div>
                 </div>
-                <button>sign in</button>
+                <button onClick={() => setShowLogin(true)}>sign in</button>
             </div>
         </div>
     )
